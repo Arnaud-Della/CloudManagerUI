54540da52b7b95294ed30d9a0066310ad4e4e3b6
Arnaud <arnaud.dellam@gmail.com>
2023-09-01 00:43:42 +0200

    test3 stable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule, NbCardModule, NbSidebarModule, NbButtonModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { AuthComponent } from './auth/auth.component';
-import { MatCardModule } from '@angular/material/card';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { MainModuleModule } from './main-module/main-module.module';
 import { RegistrationComponent } from './registration/registration.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NbThemeModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
