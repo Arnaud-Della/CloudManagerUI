7d61bd38f795d75a29bd7df0c40bb16b044beae3
Arnaud <arnaud.dellam@gmail.com>
2023-09-01 04:10:21 +0200

    aaaaa

--- src/app/main-module/main-module.module.ts
+++ src/app/main-module/main-module.module.ts
@@ -5,7 +5,7 @@ import { MainModuleRoutingModule } from './main-module-routing.module';
 import { HomeComponent } from './home/home.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
-import { NbIconModule } from '@nebular/theme';
+import { NbCardModule, NbIconModule } from '@nebular/theme';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { CloudComponent } from './cloud/cloud.component';
@@ -16,7 +16,7 @@ import {MatListModule} from '@angular/material/list';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
-
+import { NbThemeModule, NbLayoutModule, NbSidebarModule } from '@nebular/theme';
 @NgModule({
   declarations: [
     HomeComponent,
@@ -35,7 +35,9 @@ import {MatInputModule} from '@angular/material/input';
     MatCardModule,
     MatListModule,
     MatFormFieldModule,
-    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule
+    MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule,
+    NbThemeModule, NbLayoutModule, NbSidebarModule,
+    NbCardModule
   ]
 })
 export class MainModuleModule { }
